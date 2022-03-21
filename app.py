94d596cc7dd18d4d055fb23473e4647113e6d5b9
SURBHI AGRAWAL <60931664+surbhiagrawal22@users.noreply.github.com>
2022-03-21 05:21:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def main():
         st.markdown(f"""<style>.reportview-container {{background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})}}.sidebar .sidebar-content {{background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})}}</style>""",unsafe_allow_html=True)
     
    
-    Classifer_name=["KNN","Random Forest"]
+    Classifer_name=["KNN","Random Forest","SVC"]
     st.sidebar.title('Mental Health Detection')
     st.sidebar.header('')
     st.sidebar.subheader("It's quick, easy and free")
